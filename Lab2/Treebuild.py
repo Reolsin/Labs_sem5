5368746954238e2c68021f4225dc1aa549078b30
Reolsin <smirhad01@gmail.com>
2021-12-24 12:12:51 +0300

    modified:   Lab2/Treebuild.py

--- Lab2/Treebuild.py
+++ Lab2/Treebuild.py
@@ -1,3 +1,6 @@
+from typing import NewType
+
+
 class BinTree:
 
     def __init__(self, val, left, right):
@@ -108,6 +111,11 @@ class DFA_Automat(Automat):
         self.end = end
         self.states = states
 
+    def copy(self):
+        new_states = {s: get_name() for s in self.states}
+        states = {new_states[s1]:{a:new_states[self.states[s1][a]] for a in self.states[s1]} for s1 in self.states}
+        return DFA_Automat(new_states[self.start], {new_states[s] for s in self.end}, states)
+
 
 symbols = set(['*', '|', '{', '}', '%', '(', ')', '?', '<', '>'])
 
@@ -184,7 +192,7 @@ def tree_builder(tokens: list):
             groot = BinTree(tokens[first+2], None, None)
             parent = groot
         else:
-            pass
+            groot = None
         for i in range(first + 3, last):
             if type(tokens[i]) == BinTree or tokens[i].is_literal():
                 if parent:
@@ -380,17 +388,14 @@ def min_DFA2(dfa: DFA_Automat, ABC) -> DFA_Automat:
 
 def DFA_passage(dfa: DFA_Automat, string: str):
     i = 0
-    buf = []
     state = dfa.start
-    while not state in dfa.end:
-        if len(string) > i:
-            state = dfa.states[state].get(string[i])
-            buf.append(string[i])
-            i += 1
-        else:
-            return ''
-
-    return ''.join(buf)
+    while len(string) > i and dfa.states[state].get(string[i]):
+        state = dfa.states[state].get(string[i])
+        i += 1
+    if state in dfa.end:
+        return i
+    else:
+        return 0
 
 
 def Tree_passage(node: BinTree, string: str, i: int) -> set:
@@ -418,8 +423,9 @@ def Tree_passage(node: BinTree, string: str, i: int) -> set:
             s = tmp
             while len(s) != 0:
                 next_pos.update(s)
+                tmp = s
                 s = set()
-                [s.update(Tree_passage(node.left, string, pos)) for pos in s]
+                [s.update(Tree_passage(node.left, string, pos)) for pos in tmp]
             next_pos.add(i)
 
         elif node.value == '?':
@@ -430,8 +436,9 @@ def Tree_passage(node: BinTree, string: str, i: int) -> set:
             s = tmp
             j = 1
             while len(s) != 0 and j != node.value.value:
-                s = {}
-                [s.update(Tree_passage(node.left, string, pos)) for pos in s]
+                tmp = s
+                s = set()
+                [s.update(Tree_passage(node.left, string, pos)) for pos in tmp]
                 j += 1
             next_pos.update(s)
         elif node.value.is_group():
@@ -439,21 +446,143 @@ def Tree_passage(node: BinTree, string: str, i: int) -> set:
 
     return next_pos
 
+
+def inversion(dfa: DFA_Automat, ABC) -> DFA_Automat:
+    new_names = {s : get_name() for s in dfa.states}
+    start_state = get_name()
+    states = {new_names[s] : {} for s in dfa.states}
+    states[start_state] = {'': [new_names[s] for s in dfa.end]}
+    for state in dfa.states:
+        for a in dfa.states[state]:
+            dest_state = new_names[dfa.states[state][a]]
+            if a in states[dest_state]:
+                states[dest_state][a].append(new_names[state])
+            else:
+                states[dest_state][a] = [new_names[state]]
+    
+    nfa = NFA_Automat('')
+    nfa.start = start_state
+    nfa.end = new_names[dfa.start]
+    nfa.states = states
+    inv_dfa = DFA_builder(nfa, ABC)
+    inv_dfa = min_DFA2(inv_dfa, ABC)
+
+    return inv_dfa
+
+
+def addition(dfa: DFA_Automat, ABC) -> DFA_Automat:
+    new_names = {s : get_name() for s in dfa.states}
+    end_state = get_name()
+    states = {end_state: {a : end_state for a in ABC}}
+    for state in dfa.states:
+        states[new_names[state]] = {}
+        for a in ABC:
+            if a in dfa.states[state]:
+                states[new_names[state]][a] = new_names[dfa.states[state][a]]
+            else:
+                states[new_names[state]][a] = end_state
+    
+    return DFA_Automat(new_names[dfa.start], set([end_state]), states)
+
+
+def gen_regex(dfa: DFA_Automat, state) -> str:
+    middle = []
+    outer_states = {}
+    for a in dfa.states[state]:
+        if dfa.states[state][a] == state:
+            middle.append(a)
+        else:
+            if dfa.states[state][a] not in outer_states:
+                outer_states[dfa.states[state][a]] = [a]
+            else:
+                outer_states[dfa.states[state][a]].append(a)
+    
+    if len(middle) > 0:
+        if len(middle) == 1 and len(middle[0]) == 1:
+            regex = middle[0] + '*'
+        else:
+            regex = '(' + '|'.join(middle) + ')*'
+    else:
+        regex = ''
+
+    end = []
+    for s in outer_states:
+        tmp = gen_regex(dfa, s)
+        if tmp != '' and len(outer_states[s]) > 1:
+            end.append('(' + '|'.join(outer_states[s]) + ')' + tmp)
+        else:
+            end.append('|'.join(outer_states[s]) + tmp)
+            
+    if state in dfa.end:
+        if len(end) > 0:
+            if len(end) == 1 and len(end[0]) == 1:
+                regex += end[0] + '?'
+            else:
+                regex += '(' + '|'.join(end) + ')?'
+    else:
+        regex += '|'.join(end)
+
+    return regex
+
+
+
+def restore(dfa: DFA_Automat) -> str:
+    dfa = dfa.copy()
+    print(dfa)
+    states = [s for s in dfa.states if not s in dfa.end and s != dfa.start]
+
+    for s1 in states:
+
+        Enter_states = {}
+        for s2 in dfa.states:
+            for a in dfa.states[s2]:
+                if dfa.states[s2][a] == s1 and s2 != s1:
+                    if s2 in Enter_states:
+                        Enter_states[s2].append(a)
+                    else:
+                        Enter_states[s2] = [a]
+
+        middle = [a for a in dfa.states[s1] if dfa.states[s1][a] == s1]
+        if len(middle) > 0:
+            if len(middle) == 1 and len(middle[0]) == 1:
+                middle = middle[0] + '*'
+            else:
+                middle = '(' + '|'.join(middle) + ')*'
+        else:
+            middle = ''
+
+        outer_states = {a:dfa.states[s1][a] for a in dfa.states[s1] if dfa.states[s1][a] != s1}
+
+        for s in Enter_states:
+            for a1 in Enter_states[s]:
+                dfa.states[s].pop(a1)
+                for a2 in outer_states:
+                    dfa.states[s][a1 + middle + a2] = outer_states[a2]
+        dfa.states.pop(s1)
+    print(dfa)
+
+    return gen_regex(dfa, dfa.start)
+
+
 'a(s{2}da)?a|ds*'
-regex = '(s*)ss'
+'aa|ds*|sa?'
+'ds*sss(s|sa?)sd'
+'ds*|da|db|ccb*dd|ccd'
+regex = 'ds*|da|db|ccb*dd|ccd'
 regex = '(' + regex + ')'
 tokens = Parser(regex)
 tree = tree_builder(tokens)
 print(tree)
 
-str = 'das'
-pos = Tree_passage(tree, str, 0)
-print(pos)
+ABC = 'asdbc'
 
 NFA = NFA_builder(tree)
-DFA = DFA_builder(NFA, 'asd')
-min2 = min_DFA2(DFA, 'asd')
-DFA_passage(min2, str)
-
-
-print(min2)
+print(NFA)
+DFA = DFA_builder(NFA, ABC)
+print(DFA)
+min = min_DFA2(DFA, ABC)
+print(min)
+print('----------------------------------------------------------------------------')
+print(addition(min, ABC))
+print(inversion(min, ABC))
+print(restore(min))
